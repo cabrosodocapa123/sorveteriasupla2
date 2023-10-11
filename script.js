06c18e2eb9152186462fc0c9494e46f1bfdb5137
cabrosodocapa123 <138122661+cabrosodocapa123@users.noreply.github.com>
2023-10-11 07:51:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+botao.addEventListener('click',function(){
+
+    var nomeCliente = nome.value;
+    var quantiaSorvetes = quantidade.value;
+    var saborSorvete = sabores.value;
+
+    /*alert("Nome: " + nomeCliente + " Qnt.: " + quantiaSorvetes + 
+    " Sabor: " + saborSorvete);*/
+
+    pedidoNome.innerHTML = "Nome: " + nomeCliente;
+    pedidoQuantia.innerHTML = "Quantia: " + quantiaSorvetes;
+    pedidoSabor.innerHTML = "Sabor: " + saborSorvete;
+
+}); 
